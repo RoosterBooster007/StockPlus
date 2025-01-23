f9ce81b924c915758927186a1a795b571077956b
RB007 <36972202+RoosterBooster007@users.noreply.github.com>
2025-01-22 23:06:07 -0500

    Git mod upload lol

---
+++ StockPlus/GASession.cs
@@ -0,0 +1,171 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace StockPlus
+{
+    public class GASession
+    {
+        private string modId;
+        private string modName;
+        private string modVersion;
+        private string measurementId;
+        private string screenTitle;
+        private string renewScreenTitle;
+        private bool sendSystemInfo;
+        private int sessionLengthMins;
+        private HttpClient HTTPclient = new HttpClient();
+        private DateTimeOffset sessionStartTime = DateTimeOffset.UtcNow;
+        private DateTimeOffset lastEng = DateTimeOffset.UtcNow;
+        private int GAHitCount = 1;
+        private int GASessionCount = 1;
+        private string GASessionID = Regex.Replace(System.Guid.NewGuid().ToString(), "[^\\d]", "").Substring(0, 10);
+
+        internal GASession(string modId, string modName, string modVersion, string measurementId, string screenTitle, string renewScreenTitle, bool sendSystemInfo, int sessionLengthMins)
+        {
+            this.modId = modId;
+            this.modName = modName;
+            this.modVersion = modVersion;
+            this.measurementId = measurementId;
+            this.screenTitle = screenTitle;
+            this.renewScreenTitle = renewScreenTitle;
+            this.sendSystemInfo = sendSystemInfo;
+            this.sessionLengthMins = sessionLengthMins;
+
+            if (isFirstRun())
+            {
+                SendUserGA("page_view", true, "&_fv=1");
+            }
+            else
+            {
+                SendUserGA("page_view", true, "");
+            }
+        }
+
+        private static string getSystemArch()
+        {
+            if (Environment.Is64BitProcess)
+            {
+                return "x86_64";
+            }
+            else
+            {
+                return "x86";
+            }
+        }
+
+        private static string getSystemBits()
+        {
+            if (Environment.Is64BitProcess)
+            {
+                return "64";
+            }
+            else
+            {
+                return "32";
+            }
+        }
+
+        private bool isFirstRun()
+        {
+            if (!PlayerPrefs.HasKey(modId + ".newUser"))
+            {
+                PlayerPrefs.SetInt(modId + ".newUser", 1);
+                PlayerPrefs.Save();
+                return true;
+            }
+            return false;
+        }
+
+        private bool recentlyUpdated()
+        {
+            if (PlayerPrefs.GetString(modId + ".v") != modVersion)
+            {
+                PlayerPrefs.SetString(modId + ".v", modVersion);
+                PlayerPrefs.Save();
+                return true;
+            }
+            return false;
+        }
+        private void startNewGASession()
+        {
+            GASessionCount++;
+            GASessionID = Regex.Replace(System.Guid.NewGuid().ToString(), "[^\\d]", "").Substring(0, 10);
+            sessionStartTime = DateTimeOffset.UtcNow;
+            lastEng = DateTime.UtcNow;
+            GAHitCount = 1;
+            SendUserGA("page_view", false, "");
+        }
+
+        public void sendGAEvent(string event_category, string event_name, Dictionary<string, string> event_params, bool isEngaged = true)
+        {
+            if (StockPlusPlugin.telemetryEnabled.Value)
+            {
+                if ((DateTime.UtcNow - lastEng).TotalMinutes >= sessionLengthMins)
+                {
+                    startNewGASession();
+                }
+
+                event_name = WebUtility.UrlEncode(event_name);
+                event_category = WebUtility.UrlEncode(event_category);
+
+                string e_params = "";
+                foreach (KeyValuePair<string, string> kvp in event_params)
+                {
+                    if (int.TryParse(kvp.Value, out int value))
+                    {
+                        e_params += "&epn." + WebUtility.UrlEncode(kvp.Key) + "=" + value;
+                    }
+                    else
+                    {
+                        e_params += "&ep." + WebUtility.UrlEncode(kvp.Key) + "=" + WebUtility.UrlEncode(kvp.Value);
+                    }
+                }
+
+                string engagedTime = "";
+                if (isEngaged)
+                {
+                    engagedTime += "&_et = " + (long)Math.Floor((DateTime.UtcNow - lastEng).TotalMilliseconds);
+                }
+
+                GAHitCount++;
+                _ = HTTPclient.GetAsync("https://www.google-analytics.com/g/collect?v=2&tid=" + measurementId + "&cid=" + SystemInfo.deviceUniqueIdentifier + "&sid=" + GASessionID + "&ul=" + CultureInfo.CurrentCulture.Name + "&sr=" + Screen.currentResolution.width + "x" + Screen.currentResolution.height + "&uap=" + WebUtility.UrlEncode(Environment.OSVersion.Platform.ToString()) + "&uam=" + WebUtility.UrlEncode(Application.version) + "&uapv=" + WebUtility.UrlEncode(modVersion) + "&en=" + event_name + "&ep.category=" + event_category + e_params + engagedTime + "&tfd=" + (long)Math.Floor((DateTime.UtcNow - sessionStartTime).TotalMilliseconds) + "&uamb=0&uaw=0&pscdl=noapi&_s=" + GAHitCount + "&sct=" + GASessionCount + "&seg=1&_ee=1&npa=0&dma=0&frm=0&are=1");
+
+                if (isEngaged)
+                {
+                    lastEng = DateTime.UtcNow;
+                }
+            }
+        }
+
+        private void SendUserGA(string e, bool startingSession, string args)
+        {
+            if (StockPlusPlugin.telemetryEnabled.Value)
+            {
+                string sysInfo = "";
+                string eventTitle = "";
+
+                if (sendSystemInfo)
+                {
+                    sysInfo += "&ep.cpu=" + SystemInfo.processorType + "&ep.gpu=" + SystemInfo.graphicsDeviceName;
+                }
+
+                if (startingSession)
+                {
+                    eventTitle = screenTitle;
+                }
+                else
+                {
+                    eventTitle = renewScreenTitle;
+                }
+
+                _ = HTTPclient.GetAsync("https://www.google-analytics.com/g/collect?v=2&tid=" + measurementId + "&cid=" + SystemInfo.deviceUniqueIdentifier + "&sid=" + GASessionID + "&ul=" + CultureInfo.CurrentCulture.Name + "&sr=" + Screen.currentResolution.width + "x" + Screen.currentResolution.height + "&uap=" + WebUtility.UrlEncode(Environment.OSVersion.Platform.ToString()) + "&uam=" + WebUtility.UrlEncode(Application.version) + "&uapv=" + WebUtility.UrlEncode(modVersion) + "&en=" + e + sysInfo + "&tfd=" + (long)Math.Floor((DateTime.UtcNow - sessionStartTime).TotalMilliseconds) + "&uaa=" + getSystemArch() + "&uab=" + getSystemBits() + "&uamb=0&uaw=0&dt=" + WebUtility.UrlEncode(eventTitle + " (v" + modVersion + ")") + "&are=1&frm=0&pscdl=noapi&seg=1&npa=0&_s=1&sct=" + GASessionCount + "&dma=0&_ss=1&_nsi=1&_ee=1" + args);
+            }
+        }
+    }
+}
---
+++ StockPlus/Plugin.cs
@@ -0,0 +1,183 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections;
+using Mirror;
+using UnityEngine;
+using System.Reflection;
+using System.Linq;
+using BepInEx.Configuration;
+using System.Collections.Generic;
+using System;
+using HutongGames.PlayMaker.Actions;
+namespace StockPlus
+{
+    [BepInPlugin(GUID, PluginName, VersionString)]
+    public class StockPlusPlugin : BaseUnityPlugin
+    {
+        private const string GUID = "RB007.plugins.StockPlus";
+        private const string PluginName = "StockPlus";
+        private const string VersionString = "1.0.0";
+        internal static Harmony Harmony;
+
+        internal static bool isHost = false;
+        public static ConfigEntry<bool> isStocking;
+        public static ConfigEntry<int> minStockThreshold;
+        public static ConfigEntry<bool> telemetryEnabled;
+        internal static float checkInterval = 20f;
+
+        internal static GASession gaSession;
+
+        private void Awake()
+        {
+            isStocking = Config.Bind("Settings", "Auto Stocking Enabled", true, "Any available items (when they are fewer than the threshold below) will be purchased automatically.");
+            minStockThreshold = Config.Bind("Settings", "Minimum Stock Threshold", 16, "When there are fewer of each item (than this float value) available, they will be added to the shopping list (and purchased).");
+            telemetryEnabled = Config.Bind("Misc", "Enable Telemetry", true, "Help me improve this mod by sending logs of small mod/game events.");
+
+            Harmony = new Harmony(GUID);
+            Harmony.PatchAll();
+
+            gaSession = new GASession(GUID, PluginName, VersionString, "G-0BREKNCPF9", "MainMenu", "InGame", true, 30);
+
+            Debug.Log("Loaded " + PluginName + " successfully!");
+        }
+
+        [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.OnStartHost))]
+        public static class OnHost
+        {
+            [HarmonyPostfix]
+            public static void Postfix(NetworkManager __instance)
+            {
+                isHost = true;
+                __instance.StartCoroutine(CheckStock());
+            }
+
+            private static IEnumerator CheckStock()
+            {
+                while (true)
+                {
+                    yield return new WaitForSeconds(checkInterval);
+
+                    if (isStocking.Value)
+                    {
+                        AddLowStock(true);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.OnStartClient))]
+        public static class OnClient
+        {
+            [HarmonyPostfix]
+            public static void Postfix(NetworkManager __instance)
+            {
+                // Allow one-click re-stocking for client players? Needs UI...
+            }
+        }
+
+        public static void AddLowStock(bool buyToo)
+        {
+            ManagerBlackboard mboard = FindFirstObjectByType<ManagerBlackboard>();
+            ProductListing plisting = FindFirstObjectByType<ProductListing>();
+            if (mboard == null || plisting == null)
+            {
+                Debug.LogWarning("Failed to add low stock to shopping list!");
+                return;
+            }
+
+            FieldInfo isSpawningField = typeof(ManagerBlackboard).GetField("isSpawning", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (isSpawningField != null)
+            {
+                bool isSpawningValue = (bool)isSpawningField.GetValue(mboard);
+                if (isSpawningValue && buyToo)
+                {
+                    return;
+                }
+            }
+
+            int count = 0;
+            int items = 0;
+            float cost = 0;
+            foreach (int prodID in plisting.availableProducts)
+            {
+                Data_Product prodData = plisting.productPrefabs[prodID].GetComponent<Data_Product>();
+                if (prodData == null || !plisting.unlockedProductTiers[prodData.productTier])
+                {
+                    return;
+                }
+
+                MethodInfo gpeInfo = typeof(ManagerBlackboard).GetMethod("GetProductsExistences", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (gpeInfo != null)
+                {
+                    int[] existences = (int[])gpeInfo.Invoke(mboard, new object[] { prodID });
+                    if (existences.Sum() < minStockThreshold.Value)
+                    {
+                        float price = prodData.basePricePerUnit * plisting.tierInflation[prodData.productTier];
+                        price *= prodData.maxItemsPerBox;
+                        price = Mathf.Round(price * 100f) / 100f;
+
+                        mboard.AddShoppingListProduct(prodID, price);
+
+                        count++;
+                        items += prodData.maxItemsPerBox;
+                        cost += price;
+                    }
+                }
+            }
+
+            if (buyToo && mboard.shoppingTotalCharge > 0)
+            {
+                BuyStock();
+            }
+
+            if (count > 0) {
+                gaSession.sendGAEvent("game", "restock", new Dictionary<string, string>() { ["boxes"] = count.ToString(), ["items"] = items.ToString(), ["cost"] = cost.ToString(), ["buy"] = buyToo.ToString() });
+            } else
+            {
+                gaSession.sendGAEvent("game", "scan", new Dictionary<string, string>() { ["threshold"] = minStockThreshold.Value.ToString(), ["buy"]  = buyToo.ToString() });
+            }
+        }
+
+        public static void BuyStock()
+        {
+            ManagerBlackboard mboard = FindFirstObjectByType<ManagerBlackboard>();
+
+            if (mboard == null)
+            {
+                Debug.LogWarning("Failed to buy shopping list!");
+                return;
+            }
+
+            mboard.BuyCargo();
+        }
+
+        [HarmonyPatch(typeof(PlayerNetwork), nameof(PlayerNetwork.OnStartClient))]
+        public static class CreateUI
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerNetwork __instance)
+            {
+                ManagerBlackboard mboard = FindFirstObjectByType<ManagerBlackboard>();
+
+                if (mboard == null)
+                {
+                    Debug.LogWarning("Failed to create " + PluginName + " UI!");
+                    return;
+                }
+
+                // Add UI in future update?
+            }
+        }
+
+        [HarmonyPatch(typeof(ApplicationQuit), nameof(ApplicationQuit.OnEnter))]
+        public static class QuitEvent
+        {
+            [HarmonyPostfix]
+            public static void Prefix(ApplicationQuit __instance)
+            {
+                gaSession.sendGAEvent("state", "session_end", new Dictionary<string, string>() { ["closed"] = "true" });
+            }
+        }
+    }
+}
